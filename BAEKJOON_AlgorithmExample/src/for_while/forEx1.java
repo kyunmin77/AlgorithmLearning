5db10ce2683dd6d505cc97d1e0c83a9b5e0698c3
kyunmin77 <geonmin77@gmail.com>
2024-05-07 23:59:19 +0900

    for 예제 1번 (수정)

--- BAEKJOON_AlgorithmExample/src/for_while/forEx1.java
+++ BAEKJOON_AlgorithmExample/src/for_while/forEx1.java
@@ -4,20 +4,17 @@ import java.util.Scanner;
 
 class forEx1 {
 	public static void main(String[] args) {
+
 		Scanner s = new Scanner(System.in);
 
-		int a = 0;
-		int b = 0;
-
-		a = s.nextInt();
-		b = s.nextInt();
-
-		if (a > b) {
-			System.out.println(">");
-		} else if (a < b) {
-			System.out.println("<");
-		} else if (a == b) {
-			System.out.println("==");
+		String temp = s.nextLine();
+		int num = Integer.parseInt(temp);
+		int result = 0;
+		
+		for (int i = 1; i < 10; i++) {
+			result = num * i;
+			System.out.println(num + " * " + i + " = " + result);
 		}
+
 	}
 }
