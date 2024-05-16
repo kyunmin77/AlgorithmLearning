0fd16c139bab6c434ac35397dc073077f53a774d
kyunmin77 <geonmin77@gmail.com>
2024-05-17 00:27:37 +0900

    A + B // EoF

--- BAEKJOON_AlgorithmExample/src/for_while/ForEx12.java
+++ BAEKJOON_AlgorithmExample/src/for_while/ForEx12.java
@@ -2,7 +2,7 @@ package for_while;
 
 import java.util.Scanner;
 
-public class ForEx11 {
+public class ForEx12 {
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
@@ -10,19 +10,13 @@ public class ForEx11 {
 		int b = 0;
 		int sum = 0;
 
-		while (true) {
+		while (s.hasNextInt()) {
 			a = s.nextInt();
 			b = s.nextInt();
-
 			sum = a + b;
 
-			if (a == 0 && b == 0) {
-				break;
-			}
 			System.out.println(sum);
+		
 		}
-
-		s.close();
-
 	}
 }
