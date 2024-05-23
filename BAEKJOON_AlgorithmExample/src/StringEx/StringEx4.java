1a2d1c06380d078b8ba0e8aa4470cc87cdea0528
kyunmin77 <geonmin77@gmail.com>
2024-05-23 12:54:24 +0900

    int  <= char  ->아스키코드

---
+++ BAEKJOON_AlgorithmExample/src/StringEx/StringEx4.java
@@ -0,0 +1,11 @@
+package StringEx;
+
+import java.util.Scanner;
+
+public class StringEx4 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int chr = s.nextLine().charAt(0);
+		System.out.println(chr);
+	}
+}
